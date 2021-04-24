f547f8f2b4abdb96a619b8f130a796f3245f864e
lufemas <maschietto.jr@gmail.com>
2021-04-23 22:00:38 -0400

    draggable modal with css

--- src/components/SchemaViewer.js
+++ src/components/SchemaViewer.js
@@ -1,124 +1,131 @@
-import '../styles/tree.scss'
-import React, { useContext, useState } from 'react'
-import { SchemaContext } from '../contexts/SchemaContext'
-import AddModal from './AddModal'
+import "../styles/tree.scss";
+import React, { useContext, useState } from "react";
+import { SchemaContext } from "../contexts/SchemaContext";
+import AddModal from "./AddModal";
 
 export default function SchemaViewer() {
-  const [showAddModal, setShowAddModal] = useState(false)
-  const [selectedNodeId, setSelectedNodeId] = useState("")
+  const [showAddModal, setShowAddModal] = useState(false);
+  const [selectedNodeId, setSelectedNodeId] = useState("");
 
-  const { jsonSchema, setJsonSchema, deleteField } = useContext(SchemaContext)
+  const { jsonSchema, setJsonSchema, deleteField } = useContext(SchemaContext);
 
   const generateTreeForItem = (schema, parent) => {
-    const node = {}
+    const node = {};
 
-    return schema.filter(v => v.parent === parent).map(n => {
-      return (
-        <ul class="tree">
-          <li className="section">
-            <input type="checkbox" id={n.id} />
-            <label for={n.id}>
-            {n.name} 
-            </label>
-            {n.type === "Table" 
-            ? <>
-              <sup> Table</sup> 
-              <button  
-                onClick={() => 
-                              {
-                              setSelectedNodeId(n.id)
-                              console.log(selectedNodeId)
-                              setShowAddModal(true)
-                              }
-                }>Add field
+    return schema
+      .filter((v) => v.parent === parent)
+      .map((n) => {
+        return (
+          <ul class="tree">
+            <li className="section">
+              <input type="checkbox" id={n.id} />
+              <label for={n.id}>{n.name}</label>
+              {n.type === "Table" ? (
+                <>
+                  <sup> Table</sup>
+                  <button
+                    onClick={() => {
+                      setSelectedNodeId(n.id);
+                      console.log(selectedNodeId);
+                      setShowAddModal(true);
+                    }}
+                  >
+                    Add field
+                  </button>
+                </>
+              ) : (
+                <span>: {n.type}</span>
+              )}
+
+              <button
+                onClick={() => {
+                  deleteField(n.id);
+                }}
+              >
+                Delete Scheme
               </button>
-              </>
-            : <span>: {n.type}</span>}
 
-
-            <button  
-              onClick={() => 
-                            {
-                            deleteField(n.id)
-                            }
-              }>Delete Scheme
+              <button
+                onClick={() => {
+                  deleteField(n.id);
+                }}
+              >
+                Delete Scheme
               </button>
 
+              <button
+                onClick={() => {
+                  deleteField(n.id);
+                }}
+              >
+                Populate
+              </button>
 
               {generateTreeForItem(schema, n.id)}
-
-              
             </li>
-        </ul>
-
-
-      )
-    })
+          </ul>
+        );
+      });
     // return Object.keys(item).map((rootKey, index) => {
-            //   if (typeof item[rootKey] === 'object' && item[rootKey] !== null) {
+    //   if (typeof item[rootKey] === 'object' && item[rootKey] !== null) {
 
-            //     return (
-            //       <li className="section">
-            //         <input type="checkbox" id={`schema-${rootKey}`} />
-            //         <label for={`schema-${rootKey}`}>
-            //           {rootKey} <sup>({Object.keys(item[rootKey]).length})</sup>
+    //     return (
+    //       <li className="section">
+    //         <input type="checkbox" id={`schema-${rootKey}`} />
+    //         <label for={`schema-${rootKey}`}>
+    //           {rootKey} <sup>({Object.keys(item[rootKey]).length})</sup>
 
-            //           {
-            //           keyPath.length == 0 
-            //           ? <button onClick=
-            //           {
-            //             ()=>setNodeInChange({name : rootKey, path: []})
-            //           }>
-            //               Populate
-            //             </button> 
-            //           : null
-            //           }
+    //           {
+    //           keyPath.length == 0
+    //           ? <button onClick=
+    //           {
+    //             ()=>setNodeInChange({name : rootKey, path: []})
+    //           }>
+    //               Populate
+    //             </button>
+    //           : null
+    //           }
 
-            //           <button onClick={() => {
-            //             setNodeInChange({name : rootKey, path: keyPath})
-            //             setShowAddModal(true)
-            //           }
-            //           }
-            //           >Add Field</button>
-            //            <button  onClick={() => {
-            //             setNodeInChange({name : rootKey, path: keyPath})
-            //             // setShowAddModal(true)
-            //             deleteField({name : rootKey, path: keyPath})
-            //           }
-            //           }
-            //           >Delete Scheme</button>
-            //         </label>
+    //           <button onClick={() => {
+    //             setNodeInChange({name : rootKey, path: keyPath})
+    //             setShowAddModal(true)
+    //           }
+    //           }
+    //           >Add Field</button>
+    //            <button  onClick={() => {
+    //             setNodeInChange({name : rootKey, path: keyPath})
+    //             // setShowAddModal(true)
+    //             deleteField({name : rootKey, path: keyPath})
+    //           }
+    //           }
+    //           >Delete Scheme</button>
+    //         </label>
 
-            //         <ul>{generateTreeForItem(jsonSchema, null)}</ul>
+    //         <ul>{generateTreeForItem(jsonSchema, null)}</ul>
 
-            //       </li>
-            //     )
+    //       </li>
+    //     )
 
-            //   } else {
-            //     // keyPath = []
+    //   } else {
+    //     // keyPath = []
 
-            //     return (
-            //       <li>{rootKey}: <span>{item[rootKey]}</span></li>
-            //     )
-            //   }
+    //     return (
+    //       <li>{rootKey}: <span>{item[rootKey]}</span></li>
+    //     )
+    //   }
 
-            // })
-          }
+    // })
+  };
 
   return (
-          <div>
-            <AddModal setIsOpen={setShowAddModal} isOpen={showAddModal} selectedNodeId={selectedNodeId} />
+    <div>
+      <AddModal
+        setIsOpen={setShowAddModal}
+        isOpen={showAddModal}
+        selectedNodeId={selectedNodeId}
+      />
 
-
-            {
-
-              generateTreeForItem(jsonSchema, "")
-
-            }
-
-
-
-
-          </div>
-  )
+      {generateTreeForItem(jsonSchema, "")}
+    </div>
+  );
 }
--- src/ui-components/Modal.js
+++ src/ui-components/Modal.js
@@ -1,18 +1,49 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./styles/modal.scss";
 import "./styles/behaviors.scss";
 export default function Modal({ children, title, toClose }) {
+
+  const [mouseMovement, SetMouseMovement] = useState({x: 0, y: 0})
+  const [trackingMouse, SetTrackingMouse] = useState(false)
+
+  const mouseMove = (e)=>{
+    if (!trackingMouse) return
+    const panel = e.target.parentElement
+    if(panel.classList != "panel") return
+    console.log(panel)
+    // panel.offsetTop = mouseMove.y
+    panel.style.left = String(panel.offsetLeft +  mouseMovement.x ) + "px"
+    panel.style.top = String(panel.offsetTop +  mouseMovement.y) + "px"
+    // document.body.style.left
+    SetMouseMovement({x : e.movementX , y: e.movementY})
+
+  }
+
+  const dragMouseDown = (e) => {
+    SetTrackingMouse(true)
+    // console.log($panel)
+  }
+
+  const releaseDrag = (e)=>{
+    SetTrackingMouse(false)
+
+  }
+
+  useEffect(() => {
+    return () => {
+    }
+  }, [mouseMovement])
 
   const modalClassName = "modal";
   return (
     <div className={modalClassName}>
       <div className="container">
         <div className="panel">
-          <h3 className="header">
+          <h3 className="header" onMouseDown={dragMouseDown} onMouseUp={releaseDrag} onMouseMove={mouseMove} onMouseLeave={releaseDrag}>
               <span className="title">{title}</span>
-              <span className="controls">
+              <nav className="controls">
                   <button className="control-button" onClick={()=> toClose()}>X</button>
-              </span>
+              </nav>
               </h3>
 
           <div className="body">{children}</div>
